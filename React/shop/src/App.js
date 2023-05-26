c39f0971a39898c9888104c40860ffc065f3ac5a
ICHIBAN1001 <lji941001@g.yju.ac.kr>
2023-05-27 00:54:38 +0900

    UPDATE

--- React/shop/src/App.js
+++ React/shop/src/App.js
@@ -33,23 +33,7 @@ function App() {
                 )
               })
             }
-            {/* <div className="col-md-4">
-              <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%" />
-              <h4>{shoes[0].title}</h4>
-              <p>상품정보</p>
-            </div>
-
-            <div className="col-md-4">
-              <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%" />
-              <h4>상품명</h4>
-              <p>상품정보</p>
-            </div>
-
-            <div className="col-md-4">
-              <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%" />
-              <h4>상품명</h4>
-              <p>상품정보</p>
-            </div> */}
+     
         
           </div>
       </div>
